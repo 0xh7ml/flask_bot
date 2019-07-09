ae0b64f7fd6368fdad125304e727a7062ae4ce05
Md Saikat <42938253+0xh7ml@users.noreply.github.com>
2019-07-09 20:42:45 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route('/')
-def eyata():
-      return '<center><h1> Hello world </h1></center>'
-if __name__ == '__main__':
-      app.run()
+@app.route('/' ,methods = ['GET'])
+def verify():
+    if request.args.get('hub.mode') == "subscribe" and request.args.get('hub.challange'):
+        if not request.args.get('hub.verify_token') == "diyabot":
+            return "token mismatch" , 403 
+        return request.args["hub.challange"] , 200
+    return '<center> <h1> Successfully Run </h1></center>',200
+
+
+if __name__ == "__main__":
+    app.run()
