9a0d522580a0b51692c5f12b8226c1cbcfc560a1
Md Saikat <42938253+0xh7ml@users.noreply.github.com>
2019-07-09 23:00:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
-from flask import Flask,request
+import random
+from flask import Flask, request
+from pymessenger.bot import Bot
 
 app = Flask(__name__)
+ACCESS_TOKEN = 'EAAhQXDPGU8IBAAbn36dzJ2BAbH8S2aPeZApSH8LQO0Wre3j4kqjJTfzVi53clOK7BaujcjHG3ZBD1RtnFjeQqhmzODaaw4KgEZCDnpkQswX9jlrnR5TVMIG73Mvkjnhk1oJmJzmUZCEzICE9DALGdd5G5AYxjezQUgrWmFZCclAZDZD'
+VERIFY_TOKEN = 'diyabot'
+bot = Bot(ACCESS_TOKEN)
 
-@app.route('/' ,methods = ['GET'])
-def verify():
-    if request.args.get('hub.mode') == "subscribe" and request.args.get('hub.challange'):
-        if not request.args.get('hub.verify_token') == "diyabot":
-            return "token mismatch" , 403 
-        return request.args["hub.challange"] , 200
-    return '<center> <h1> Successfully Run </h1></center>',200
-
-
+#We will receive messages that Facebook sends our bot at this endpoint 
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+        if request.method == 'GET':
+            token_sent = request.args.get("hub.verify_token")
+            return verify_fb_token(token_sent)
+        else:
+            output = request.get_json()
+            for event in output['entry']:
+                messaging = event['messaging']
+                for message in messaging:
+                    if message.get('message'):
+                        recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
+        return "Message Processed"
+def verify_fb_token(token_sent):
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+def get_message():
+    sample_responses = ["Hello,How are u?","Saikat is Currently offline, Im Diyabot", "Are u Okey?","I love u"]
+    return random.choice(sample_responses)
+def send_message(recipient_id, response):
+    bot.send_text_message(recipient_id, response)
+    return "success"
 if __name__ == "__main__":
     app.run()
